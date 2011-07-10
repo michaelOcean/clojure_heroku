97e5d3bc60ef83b8e5cbd54810b29f35e0197f64
Michael Chu <michael.ocean.chu@gmail.com>
2011-07-09 18:15:37 -0700

    init

---
+++ project.clj
@@ -0,0 +1,4 @@
+(defproject clojure-heroku "0.0.1"
+  :dependencies
+    [[org.clojure/clojure "1.2.1"]
+     [ring/ring-jetty-adapter "0.3.9"]])
---
+++ test/clojure_heroku/test/core.clj
@@ -0,0 +1,6 @@
+(ns clojure_heroku.test.core
+  (:use [clojure_heroku.core])
+  (:use [clojure.test]))
+
+(deftest replace-me ;; FIXME: write
+  (is false "No tests have been written."))
