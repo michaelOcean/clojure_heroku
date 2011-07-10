269217bbdb03893a906ddb79d17e9c8b91773013
Michael Chu <michael.ocean.chu@gmail.com>
2011-07-09 23:03:30 -0700

    test update

--- project.clj
+++ project.clj
@@ -1,4 +1,9 @@
 (defproject clojure-heroku "0.0.1"
-  :dependencies
-    [[org.clojure/clojure "1.2.1"]
-     [ring/ring-jetty-adapter "0.3.9"]])
+ :dependencies
+    [[org.clojure/clojure "1.1.0"]
+     [org.clojure/clojure-contrib "1.1.0"]
+     [ring/ring-core "0.2.0"]
+     [ring/ring-jetty-adapter "0.2.0"]]
+  :dev-dependencies
+  [[ring/ring-devel "0.2.0"]
+   [swank-clojure "1.2.1"]])
--- src/demo/web.clj
+++ src/demo/web.clj
@@ -1,10 +1,44 @@
 (ns demo.web
   (:use ring.adapter.jetty))
 
-(defn app [req]
-  {:status 200
-   :headers {"Content-Type" "text/plain"}
-   :body "Hello from Clojure!\n"})
+(defn handler [req]
+  {:status  200
+   :headers {"Content-Type" "text/html"}
+   :body "<p> Clojure is fun! </p>
+         <script src='http://widgets.twimg.com/j/2/widget.js'></script>
+<script>
+new TWTR.Widget({
+  version: 2,
+  type: 'profile',
+  rpp: 4,
+  interval: 6000,
+  width: 250,
+  height: 300,
+  theme: {
+    shell: {
+      background: '#432e6b',
+      color: '#ffffff'
+    },
+    tweets: {
+      background: '#170421',
+      color: '#ccc6cc',
+      links: '#8d6fab'
+    }
+  },
+  features: {
+    scrollbar: false,
+    loop: false,
+    live: false,
+    hashtags: true,
+    timestamp: true,
+    avatars: false,
+    behavior: 'all'
+  }
+}).render().setUser('michaelOceanChu').start();
+</script>"})
+
+(def app
+  (-> #'handler))
 
 (defn -main []
   (let [port (Integer/parseInt (System/getenv "PORT"))]
